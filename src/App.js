24ac4aabea72157772823c205f2238d63ec7e0cb
nauval01~ <nauvalmulyana242@gmail.com>
2023-02-17 14:53:04 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import {Navbar} from 'react-bootstrap'
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <h1>HAllo kontol</h1>
+    </div>
+  );
+}
+
+export default App;
