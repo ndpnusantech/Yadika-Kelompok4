7da9f7df5118658644ebeae4feaf708f61a03b70
nauval01~ <nauvalmulyana242@gmail.com>
2023-03-09 22:53:22 +0700

    nauval - fix cart, order, add profile page

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import ProductDetail from "./pages/ProductDetail";
 import Register from "./pages/Register"
 import Cart from "./pages/Cart"
 import Order from "./pages/Order";
+import Profile from "./pages/Profile";
 import './asset/css/Index.css'
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
         <Route path="/product" element={< Product/>} />
         <Route path="/login" element={< Login/>} />
         <Route path="/register" element={< Register/>} />
+        <Route path="/profile" element={< Profile/>} />
         <Route path="/productDetail" element={< ProductDetail/>} />
         <Route path="/cart" element={< Cart/>} />
         <Route path="/order" element={< Order/>} />
---
+++ src/components/UserProfile.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {  } from "react-bootstrap";
+
+const UserProfile = () => {
+  return (
+    <div>
+        <img src="../image/profile.jpg" className='border border-5 rounded-circle' width={100} alt="Profile" />
+    </div>
+)
+}
+
+export default UserProfile
